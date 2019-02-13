18fde09a4c4fc25c486617159ed4d679145b6433
Andrew <andrew@pixman.cz>
2019-02-13 11:17:53 +0100

    Some changes

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php 
-    require '../content/lang_ru.php';
+    require ('../content/lang_ru.php');
 ?>
 
 <!DOCTYPE html>
@@ -13,17 +13,53 @@
 </head>
 <body>
     <form class="form" method="POST" action="#">
-        <?php
-            foreach($lang as $key => $item) {
-        ?>
-                    <div class="form__input form__input--lvl1">
-                        <label><?= $key; ?></label>
-                        <input type="text" placeholder='<?= $item; ?>' value='<?= $item; ?>'>
-                    </div>
- 
-        <?php  
-            }
-        ?>
+        <lenged>Menu</legend>
+
+        <fieldset>
+            <?php foreach($lang['menu'] as $key => $item):
+                foreach($item as $k => $v):
+                    if($k == 'url') continue;
+            ?>
+                <div class="form__input form__input--lvl1">
+                    <label><?= $k; ?></label>
+                    <input type="text" name="<?= $k; ?>" placeholder='<?= $v; ?>' value='<?= $v; ?>'>
+                </div>
+            <?php 
+                endforeach;
+                endforeach;
+            ?>
+            <button type="submit">Save changes</button>
+        </fieldset>
     </form>
+
+    <form class="form2" method="POST" action="#">
+        <p>Services</p>
+
+        <?php foreach($lang['servicesList'] as $key => $item):
+            foreach($item as $k => $v):
+                if($k == 'id') continue;
+        ?>
+            <div class="form__input form__input--lvl1">
+                <label><?= $k; ?></label>
+                <input type="text" placeholder='<?= $v; ?>' value='<?= $v; ?>'>
+            </div>
+        <?php 
+            endforeach;
+            endforeach;
+        ?>
+ 
+        <button type="submit">Save changes</button>
+    </form>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $('[type="submit"]').on('click', function(e) {
+            e.preventDefault();
+        
+            const form = $(this).closest('form');
+            const formArray = form.serializeArray();
+
+        });
+    </script>
 </body>
 </html>
